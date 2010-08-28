36f1963d29b2175bcc089be90d841a4dc94f8989
vovasty <vovasty@aramzamzam.net>
2010-08-28 15:17:58 +0400

    code cleanup

--- Classes/DocumentViewController.m
+++ Classes/DocumentViewController.m
@@ -88,13 +88,6 @@
     return YES;
 }
 
-- (void)didReceiveMemoryWarning {
-        // Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-    
-        // Release any cached data, images, etc that aren't in use.
-}
-
 - (void)viewDidUnload {
     [super viewDidUnload];
     [attachmentsViewController release];
