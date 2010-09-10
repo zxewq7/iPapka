e6237e031d00ff6d246efe12c3c993e6bb3b0fa4
vovasty <vovasty@aramzamzam.net>
2010-09-10 18:40:08 +0400

    code cleanup

--- Classes/AppDelegate.h
+++ Classes/AppDelegate.h
@@ -22,8 +22,6 @@
 
 @property (nonatomic, retain) IBOutlet UIWindow                 *window;
 
-@property (nonatomic, retain) IBOutlet UIViewController         *viewController;
-
 @property (nonatomic, retain) IBOutlet RootViewController       *rootViewController;
 @end
 
