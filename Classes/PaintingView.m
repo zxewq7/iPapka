b0e5769a606a9b3195a16bffaa47cb8835002978
vovasty <vovasty@aramzamzam.net>
2010-09-16 20:43:55 +0400

    more clever code for image retrieving

--- Classes/PaintingView.m
+++ Classes/PaintingView.m
@@ -93,19 +93,34 @@
     glGetRenderbufferParameterivOES(GL_RENDERBUFFER_OES, GL_RENDERBUFFER_HEIGHT_OES, &backingHeight);
     
     NSInteger x = 0, y = 0, width = backingWidth, height = backingHeight;
-    NSInteger dataLength = width * height * 4;
-    GLubyte *data = (GLubyte*)malloc(dataLength * sizeof(GLubyte));
+    
+    int bitsPerComponent    = 8;
+    int bitsPerPixel        = 32;
+    int bytesPerPixel       = (bitsPerComponent * 4)/8;
+    int bytesPerRow         = bytesPerPixel * width;
+    NSInteger dataLength    = bytesPerRow * height;
+    
+    NSMutableData *buffer   = [[NSMutableData alloc] initWithCapacity:dataLength];
+    
+    if( ! buffer ) 
+    {
+        NSLog(@"PaintingView#image: not enough memory");
+        [buffer release];
+        return nil;
+    }
     
         // Read pixel data from the framebuffer
-    glPixelStorei(GL_PACK_ALIGNMENT, 1);
-    glReadPixels(x, y, width, height, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    glFinish();
+    glPixelStorei(GL_PACK_ALIGNMENT, 4);    /* Force 4-byte
+                                             alignment */
+    glReadPixels(x, y, width, height, GL_RGBA, GL_UNSIGNED_BYTE, [buffer mutableBytes]);
     
         // Create a CGImage with the pixel data
         // If your OpenGL ES content is opaque, use kCGImageAlphaNoneSkipLast to ignore the alpha channel
         // otherwise, use kCGImageAlphaPremultipliedLast
-    CGDataProviderRef ref = CGDataProviderCreateWithData(NULL, data, dataLength, NULL);
+    CGDataProviderRef ref = CGDataProviderCreateWithData(NULL, [buffer mutableBytes], dataLength, NULL);
     CGColorSpaceRef colorspace = CGColorSpaceCreateDeviceRGB();
-    CGImageRef iref = CGImageCreate(width, height, 8, 32, width * 4, colorspace, kCGBitmapByteOrder32Big | kCGImageAlphaPremultipliedLast,
+    CGImageRef iref = CGImageCreate(width, height, bitsPerComponent, bitsPerPixel, bytesPerRow, colorspace, kCGBitmapByteOrder32Big | kCGImageAlphaPremultipliedLast,
                                     ref, NULL, true, kCGRenderingIntentDefault);
     
         // OpenGL ES measures data in PIXELS
@@ -141,7 +156,7 @@
     UIGraphicsEndImageContext();
     
         // Clean up
-    free(data);
+    [buffer release];
     CFRelease(ref);
     CFRelease(colorspace);
     CGImageRelease(iref);
