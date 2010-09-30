7a4b2fac7d22d15982a9cf851b3793471bb0a1da
vovasty <vovasty@aramzamzam.net>
2010-09-30 21:19:13 +0400

    forgotten reference to ResolutionManaged

--- Classes/LNDocumentWriter.m
+++ Classes/LNDocumentWriter.m
@@ -11,7 +11,7 @@
 #import "LNHttpRequest.h"
 #import "DocumentManaged.h"
 #import "SBJsonWriter.h"
-#import "Resolution.h"
+#import "ResolutionManaged.h"
 #import "DataSource.h"
 
 static NSString* OperationCount = @"OperationCount";
@@ -153,9 +153,9 @@ static NSString* OperationCount = @"OperationCount";
     
     [dictDocument setObject:action forKey:@"action"];
     
-    if ([document isKindOfClass: [Resolution class]])
+    if ([document isKindOfClass: [ResolutionManaged class]])
     {
-        Resolution *resolution = (Resolution *) document;
+        ResolutionManaged *resolution = (ResolutionManaged *) document;
         
         if (resolution.deadline)
             [dictDocument setObject:[parseFormatterSimple stringFromDate:resolution.deadline] forKey:@"deadline"];
