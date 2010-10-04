20ee704bb515b5a8e65971aa33f9033fbaef2b7b
vovasty <vovasty@aramzamzam.net>
2010-10-04 16:01:47 +0400

    keychain for login/password

--- Classes/PasswordManager.m
+++ Classes/PasswordManager.m
@@ -8,7 +8,7 @@
 
 #import "PasswordManager.h"
 #import "SynthesizeSingleton.h"
-
+#import "KeychainItemWrapper.h"
 
 #define kLoginFieldTag 1001
 #define kPasswordFieldTag 1002
@@ -17,14 +17,26 @@
 SYNTHESIZE_SINGLETON_FOR_CLASS(PasswordManager);
 -(void) credentials:(void (^)(NSString *login, NSString *password, BOOL canceled))handler
 {
+    KeychainItemWrapper *wrapper = [[KeychainItemWrapper alloc] initWithIdentifier:@"Password" accessGroup:nil];
+    
+    NSString *login = [wrapper objectForKey:(NSString *)kSecAttrAccount];
+    NSString *password = [wrapper objectForKey:(NSString *)kSecValueData];
+    
+    [wrapper release];
+
+    if (login && password && ![login isEqualToString:@""] && ![password isEqualToString:@""])
+    {
+        if (handler)
+            handler(login, password, NO);
+        return;
+    }
+    
     [passwordDialogHandler release];
     
     passwordDialogHandler = [handler copy];
     
     UITextField *textField;
     UITextField *textField2;
-    NSString *login = nil;
-    NSString *passwrod = nil;
     
     UIAlertView *prompt = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Username and password", "Username and password")
                                                      message:@"\n\n\n" // IMPORTANT
@@ -44,7 +56,7 @@ SYNTHESIZE_SINGLETON_FOR_CLASS(PasswordManager);
     [textField2 setBackgroundColor:[UIColor whiteColor]];
     [textField2 setPlaceholder:NSLocalizedString(@"Password", "Password")];
     [textField2 setSecureTextEntry:YES];
-    textField2.text = passwrod;
+    textField2.text = password;
     textField2.tag = kPasswordFieldTag;
     [prompt addSubview:textField2];
     
@@ -78,8 +90,19 @@ SYNTHESIZE_SINGLETON_FOR_CLASS(PasswordManager);
     NSString *login = loginField.text;
     NSString *password = passwordField.text;
 
+    BOOL canceled = (buttonIndex == 0);
+    
+    if (!canceled)
+    {
+        KeychainItemWrapper *wrapper = [[KeychainItemWrapper alloc] initWithIdentifier:@"Password" accessGroup:nil];
+        [wrapper setObject:login forKey: (NSString *)kSecAttrAccount];
+        [wrapper setObject:password forKey: (NSString *)kSecValueData];
+        [wrapper release];
+        
+    }
+    
     if (passwordDialogHandler)
-        passwordDialogHandler(login, password, buttonIndex == 0);
+        passwordDialogHandler(login, password, canceled);
     
     [passwordDialogHandler release];
     passwordDialogHandler = nil;
