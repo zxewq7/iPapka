11bbf6f7b696eb44f175fc4963fef5eb18cba915
vovasty <vovasty@aramzamzam.net>
2010-10-26 12:37:20 +0400

    fetching logo

--- Classes/LNSettingsReader.m
+++ Classes/LNSettingsReader.m
@@ -8,6 +8,13 @@
 
 #import "LNSettingsReader.h"
 
+@interface LNSettingsReader(Private)
+
+-(void) removeBlankLogo;
+-(NSString*) blankLogoPath;
+
+@end
+
 @implementation LNSettingsReader
 -(void) sync
 {
@@ -38,8 +45,62 @@
         
         [currentDefaults synchronize];
         
+        NSDictionary *resolution = [response objectForKey:@"resolution"];
+        NSString *blankLogoUrl = [resolution objectForKey:@"logo"];
+        NSString *blankLogoVersion = [resolution objectForKey:@"version"];
+        
+        NSString *currentBlankLogoVersion = [currentDefaults valueForKey:@"blankLogoVersion"];
+        
+        if (blankLogoUrl)
+        {
+            if (![currentBlankLogoVersion isEqualToString:blankLogoVersion])
+            {
+
+                [blockSelf fileRequestWithUrl:[[blockSelf serverUrl] stringByAppendingString:blankLogoUrl]
+                                         path:[blockSelf blankLogoPath]
+                                   andHandler:^(BOOL error, NSString* path)
+                                   {
+                                       if (error)
+                                           [blockSelf removeBlankLogo];
+
+                                       else
+                                       {
+                                           [currentDefaults setObject:blankLogoVersion forKey:@"blankLogoVersion"];
+                                           [currentDefaults setObject:path forKey:@"blankLogoPath"];
+                                           [currentDefaults synchronize];
+                                       }
+                                   }];
+            }
+        }
+        else
+            [blockSelf removeBlankLogo];
     }];
     
     [self endSession];
 }
+
+#pragma mark Private
+
+-(void) removeBlankLogo
+{
+    NSUserDefaults *currentDefaults = [NSUserDefaults standardUserDefaults];
+    NSFileManager *df = [NSFileManager defaultManager];
+
+    [currentDefaults removeObjectForKey:@"blankLogoVersion"];
+    [currentDefaults removeObjectForKey:@"blankLogoPath"];
+    [df removeItemAtPath:[self blankLogoPath] error:NULL];
+    [currentDefaults synchronize];
+    
+}
+
+-(NSString *) blankLogoPath
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    
+    NSString *blankLogoPath = ([paths count] > 0) ? [paths objectAtIndex:0] : nil;
+    blankLogoPath = [blankLogoPath stringByAppendingPathComponent:@"BlankLogo.png"];
+   
+    return blankLogoPath;
+}
+
 @end
