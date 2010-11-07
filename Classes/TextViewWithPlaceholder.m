e53ea61b638cc08168d5f5b36b7c94bd17f2f31a
Vladimir Solomenchuk <vovasty@aramzamzam.net>
2010-11-07 15:06:20 +0300

    removed bottom offset

--- Classes/TextViewWithPlaceholder.m
+++ Classes/TextViewWithPlaceholder.m
@@ -21,10 +21,13 @@
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textChanged:) name:UITextViewTextDidChangeNotification object:nil];
 }
 
-- (id)initWithFrame:(CGRect)frame{
-    if(self = [super initWithFrame:frame]){
+- (id)initWithFrame:(CGRect)frame
+{
+    if(self = [super initWithFrame:frame])
+    {
         [self setPlaceholder:@""];
         [self setPlaceholderColor:[UIColor lightGrayColor]];
+        
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textChanged:) name:UITextViewTextDidChangeNotification object:nil];
     }
     return self;
@@ -58,6 +61,37 @@
     if( [[self text] length] == 0 && [[self placeholder] length] > 0 )
         [[self viewWithTag:999] setAlpha:1];
     [super drawRect:rect];
+}
+
+//fix bottom margins
+//http://www.hanspinckaers.com/multi-line-uitextview-similar-to-sms
+-(void)setContentOffset:(CGPoint)s
+{
+	if(self.tracking || self.decelerating){
+		//initiated by user...
+		self.contentInset = UIEdgeInsetsMake(0, 0, 0, 0);
+	} else {
+        
+		float bottomOffset = (self.contentSize.height - self.frame.size.height + self.contentInset.bottom);
+		if(s.y < bottomOffset && self.scrollEnabled){
+			self.contentInset = UIEdgeInsetsMake(0, 0, 8, 0); //maybe use scrollRangeToVisible?
+		}
+		
+	}
+	
+	[super setContentOffset:s];
+}
+
+-(void)setContentInset:(UIEdgeInsets)s
+{
+	UIEdgeInsets insets = s;
+	
+	if(s.bottom>8) 
+        insets.bottom = 0;
+    
+	insets.top = 0;
+    
+	[super setContentInset:insets];
 }
 
 - (void) dealloc {
